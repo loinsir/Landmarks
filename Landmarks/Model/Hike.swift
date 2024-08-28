7b5c4eb317cd7ec20fd1f2c8b06cb7f87d83f6b9
Inhwan Kim <a9327370@gmail.com>
2024-08-29 00:51:41 +0900

    Chapter 2

---
+++ Landmarks/Model/Hike.swift
@@ -0,0 +1,30 @@
+//
+//  Hike.swift
+//  Landmarks
+//
+//  Created by 김인환 on 8/29/24.
+//
+
+import Foundation
+
+struct Hike: Codable, Identifiable {
+    var id: Int
+    var name: String
+    var distance: Double
+    var difficulty: Int
+    var observations: [Observation]
+    
+    static var formatter = LengthFormatter()
+    
+    var distanceText: String {
+        Hike.formatter.string(fromValue: distance, unit: .kilometer)
+    }
+    
+    struct Observation: Codable, Hashable {
+        var distanceFromStart: Double
+        
+        var elevation: Range<Double>
+        var pace: Range<Double>
+        var heartRate: Range<Double>
+    }
+}
---
+++ Landmarks/Views/Badges/Badge.swift
@@ -0,0 +1,33 @@
+//
+//  Badge.swift
+//  Landmarks
+//
+//  Created by 김인환 on 8/29/24.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    var badgeSymbols: some View {
+        ForEach(0..<8) { index in
+            RotatedBadgeSymbol(angle: Angle(degrees: Double(index) / Double(8)) * 360.0)
+        }
+    }
+    
+    var body: some View {
+        ZStack {
+            BadgeBackground()
+            
+            GeometryReader { geometry in
+                badgeSymbols
+                    .scaleEffect(1.0 / 4.0, anchor: .top)
+                    .position(x: geometry.size.width / 2.0, y: (3.0 / 4.0) * geometry.size.height)
+            }
+        }
+        .scaledToFit()
+    }
+}
+
+#Preview {
+    Badge()
+}
